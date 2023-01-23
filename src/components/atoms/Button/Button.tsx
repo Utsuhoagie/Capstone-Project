1e60aa311c5af76e055f6598a0a8ef30f8fd474c
Utsuhoagie <thiendangssb@gmail.com>
2023-01-24 00:31:16 +0700

    component: Button

---
+++ src/components/atoms/Button/Button.tsx
@@ -0,0 +1,34 @@
+import { buttonMapping } from './ButtonMapping';
+
+type ButtonProps = React.HTMLAttributes<HTMLButtonElement> & {
+	children: React.ReactNode;
+	width: 'full' | 'small' | 'medium' | 'big';
+	secondary?: boolean;
+	ref?: React.RefObject<HTMLButtonElement>;
+};
+
+export const Button = ({
+	children,
+	width,
+	secondary,
+	...props
+}: ButtonProps) => {
+	const classes =
+		buttonMapping.all +
+		(secondary ? buttonMapping.secondary : buttonMapping.primary) +
+		buttonMapping[width].width;
+
+	return (
+		<button className={classes} {...props}>
+			{children}
+		</button>
+	);
+};
+
+// const Thing = () => {
+// 	return (
+// 		<Button className='rounded opacity-5 shadow shadow-primary-normal'>
+// 			aaa
+// 		</Button>
+// 	);
+// };
---
+++ src/components/atoms/Button/ButtonMapping.ts
@@ -0,0 +1,21 @@
+// Regular styles only change based on width/secondary
+// --
+// Other styles are state styles. e.g: hover, disabled
+// and can be included always
+
+export const buttonMapping = {
+	all:
+		' rounded cursor-pointer px-6 py-2 ' +
+		' hover:shadow hover:shadow-primary-normal ' +
+		' active:shadow active:shadow-primary-normal ' +
+		' disabled:opacity-50 disabled:cursor-not-allowed ',
+
+	primary: ' bg-primary-normal text-neutral-gray-1 active:bg-primary-dark-2 ',
+	secondary:
+		' bg-neutral-gray-1 text-neutral-gray-9 border border-primary-normal active:bg-primary-bright-2 ',
+
+	full: { width: ' w-button-full ' },
+	small: { width: ' w-button-small ' },
+	medium: { width: ' w-button-medium ' },
+	big: { width: ' w-button-big ' },
+};
